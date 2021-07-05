caa59b53deb55efd10991c8152d2fd2cbc0ce459
Jessy <dreamjessy0715@gmail.com>
2021-07-05 11:14:30 +0800

    reset、部分使用 elementUI

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <el-button type="primary">我是按鈕</el-button>
     <router-view/>
   </div>
 </template>
