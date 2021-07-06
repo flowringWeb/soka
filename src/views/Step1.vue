a54ddb243b505e7c2f2912e3c00d50e77e81798e
Jessy <dreamjessy0715@gmail.com>
2021-07-06 11:21:46 +0800

    表單優化

--- src/views/Step1.vue
+++ src/views/Step1.vue
@@ -28,10 +28,10 @@ export default {
         <el-col :span="8">
           <el-form ref="form" :model="form" :inline="true" label-width="auto" size="large" :label-position="labelPosition">
             <el-form-item label="組織劃分案名">
-              <el-input v-model="form.orgName"></el-input>
+              <el-input v-model="form.orgName" clearable></el-input>
             </el-form-item>
             <el-form-item label="組織劃分案編號">
-              <el-input v-model.number="form.orgnNo"></el-input>
+              <el-input v-model.number="form.orgnNo" clearable></el-input>
             </el-form-item>
             <el-form-item label="劃分建議生效月">
               <el-col :span="11">
@@ -43,16 +43,16 @@ export default {
               </el-col>
             </el-form-item>
             <el-form-item label="全區會">
-              <el-input v-model="form.dct"></el-input>
+              <el-input v-model="form.dct" clearable></el-input>
             </el-form-item>
             <el-form-item label="提報單位(凍結區域頂點)">
-              <el-select v-model="form.depart" placeholder="">``
+              <el-select v-model="form.depart" placeholder="請選擇" filterable allow-create>
                 <el-option label="Zone one" value="shanghai"></el-option>
                 <el-option label="Zone two" value="beijing"></el-option>
               </el-select>
             </el-form-item>
             <el-form-item label="劃分案負責人">
-              <el-input v-model="form.orgPerson"></el-input>
+              <el-input v-model="form.orgPerson" clearable></el-input>
             </el-form-item>
             <el-form-item label="立案時間">
               <el-col :span="11">
@@ -67,14 +67,27 @@ export default {
         <el-col :span="8">
           <el-form ref="form" :model="form" :inline="true" label-width="auto">
             <el-form-item label="組織劃分案說明">
-              <el-input type="textarea" v-model="form.state"></el-input>
+              <el-input type="textarea" 
+                v-model="form.state" 
+                clearable 
+                maxlength="50" 
+                show-word-limit
+                :autosize="{ minRows: 5, maxRows: 8}"
+                >
+              </el-input>
             </el-form-item>
           </el-form>
         </el-col>
         <el-col :span="8">
           <el-form ref="form" :model="form" :inline="true" label-width="auto">
             <el-form-item label="備註">
-              <el-input type="textarea" v-model="form.note"></el-input>
+              <el-input type="textarea" 
+              v-model="form.note"
+              clearable 
+              maxlength="50" 
+              show-word-limit 
+              :autosize="{ minRows: 5, maxRows: 8}"
+              ></el-input>
             </el-form-item>
           </el-form>
         </el-col>
@@ -92,6 +105,12 @@ export default {
 .step1Form {
   padding: 3rem 1rem;
 }
+
+// .el-input {
+//   /deep/ .el-input__inner {
+//       width: 15vw;
+//     }
+//   }
 </style>
 
 
