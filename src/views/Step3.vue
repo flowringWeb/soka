5121a945ba377c8dacaa84dd99bfa8aad8462fca
Jessy <dreamjessy0715@gmail.com>
2021-07-22 11:39:24 +0800

    新增 step3 表格

--- src/views/Step3.vue
+++ src/views/Step3.vue
@@ -1,10 +1,12 @@
 <script>
     import Steps from "@/components/Steps.vue";
     import ExportExcel from "@/components/ExportExcel.vue";
+    import Pagination from "@/components/Pagination.vue";
     export default {
         components: {
             Steps,
-            ExportExcel
+            ExportExcel,
+            Pagination
         },
         data() {
             return {
@@ -31,7 +33,41 @@
                     version: 3
                 },
                 //開啟地圖附件
-                dialogMapVisible: false
+                dialogMapVisible: false,
+
+                //人員遷移相關表格
+                tableMoveSource: [
+                    {
+                    check: "",
+                    id: 16666,
+                    name: "周oo",
+                    job: "地區綜合長",
+                    org: "汐止區/青山地區",
+                    old_org: "大安地區",
+                    type: "success",
+                    icon: "el-icon-check",
+                    },
+                    {
+                    check: "",
+                    id: 453534,
+                    name: "莫xx",
+                    job: "地區部長",
+                    org: "汐止區/青山地區",
+                    old_org: "大安地區",
+                    type: "danger",
+                    icon: "el-icon-delete",
+                    },
+                    {
+                    check: "",
+                    id: 5555555,
+                    name: "周oo",
+                    job: "地區綜合長",
+                    org: "汐止區/青山地區",
+                    old_org: "大安地區",
+                    type: "success",
+                    icon: "el-icon-check",
+                    },
+                ],
             }
         },
         methods: {
@@ -117,6 +153,50 @@
                 </el-col>
             </el-row>
         </div>
+        <div>
+            <el-row>
+                <el-col :span="18">
+                    <section>
+                        <div class="sub-title">(二)人員遷移</div>
+                        <h5 class="ml-2">組織單位人員</h5>
+                        <div>
+                            <el-table :data="tableMoveSource"
+                            :header-cell-style="{ background: '#eee' }" 
+                            class="mb-3">
+                                <el-table-column type="selection" label="選擇" align="center">
+                                </el-table-column>
+                                <el-table-column prop="id" label="會員編號" align="center">
+                                </el-table-column>
+                                <el-table-column
+                                prop="name"
+                                label="姓名"
+                                show-overflow-tooltip
+                                align="center"
+                                >
+                                </el-table-column>
+                                <el-table-column prop="job" label="職務名稱" align="center">
+                                </el-table-column>
+                                <el-table-column prop="org" label="單位" align="center">
+                                </el-table-column>
+                                <el-table-column prop="old_org" label="原單位" align="center">
+                                </el-table-column>
+                                <el-table-column label="退回原單位" align="center">
+                                <template slot-scope="scope">
+                                    <el-button
+                                    :type="scope.row.type"
+                                    :icon="scope.row.icon"
+                                    circle
+                                    size="small"
+                                    ></el-button>
+                                </template>
+                                </el-table-column>
+                            </el-table>
+                            <Pagination></Pagination>
+                        </div>
+                    </section>
+                </el-col>
+            </el-row>
+        </div>
 
         <!-- 地圖 dialog -->
         <el-dialog
@@ -131,6 +211,11 @@
     </div>
 </template>
 <style lang="scss" scoped>
+    h5 {
+        margin-bottom: 0.5rem;
+        padding: 0.5rem 0.1rem;
+        background-color: #ccc;
+    }
     .title {
         text-align: center;
         color: #fff;
