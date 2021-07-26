35515856f8f28968f6d4ec45e1d5b452fe6cfb97
Jessy <dreamjessy0715@gmail.com>
2021-07-26 15:50:22 +0800

    steps 回步驟

--- src/components/Steps.vue
+++ src/components/Steps.vue
@@ -13,51 +13,60 @@ export default {
           title: "step1",
           description: "建立 Project",
           icon: "el-icon-edit",
+          icon2: "el-icon-delete"
         },
         {
           title: "step2",
           description: "組織異動",
           icon: "el-icon-upload",
+          icon2: "el-icon-delete"
         },
         {
           title: "step3",
           description: "人員異動",
           icon: "el-icon-upload",
+          icon2: "el-icon-delete"
         },
         {
           title: "step4",
           description: "內部審核",
           icon: "el-icon-upload",
+          icon2: "el-icon-delete"
         },
         {
           title: "step5",
           description: "呈核報告書",
           icon: "el-icon-upload",
+          icon2: "el-icon-delete"
         },
         {
           title: "step6",
           description: "報告書簽核",
           icon: "el-icon-edit",
+          icon2: "el-icon-delete"
         },
         {
           title: "step7",
           description: "人事任命作業",
           icon: "el-icon-edit",
+          icon2: "el-icon-delete"
         },
         {
           title: "step8",
           description: "人事任命簽核",
           icon: "el-icon-edit",
+          icon2: "el-icon-delete"
         },
       ],
+      hover: false,
     }
   },
   methods: {
     //回上一步
-    stepChange(current){
-        console.log(current, this.currentStep);
-        if(current < this.currentStep){
-            this.currentStep = current;
+    stepChange(selectStep){
+        // console.log(selectStep, this.currentStep);
+        if(selectStep < this.currentStep){
+            this.currentStep = selectStep;
         }
     }
   }
@@ -78,10 +87,15 @@ export default {
               :icon="item.icon"
               :key="index"
               @click.native="stepChange(index)"
+              @mouseover.native="hover = true"
+              @mouseleave.native="hover = false"
             >
             </el-step>
+            
       </el-steps>
+      <el-button v-show="hover" type="primary" icon="el-icon-edit" plain round>回步驟</el-button>        
     </div>
 </template>
-<style lang="">
+<style lang="scss">
+
 </style>
