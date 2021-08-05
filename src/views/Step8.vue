f7681a148ee272c73c949696882be52ea5ca2bad
Jessy <dreamjessy0715@gmail.com>
2021-08-05 13:21:07 +0800

    step8 增加內容

--- src/views/Step8.vue
+++ src/views/Step8.vue
@@ -8,18 +8,30 @@
             return {
                 //steps
                 active: 7,
+                peopleCount: [
+                    {
+                        title:"區級",
+                        number:5,
+                        pTime:1,
+
+                    },
+                    {title:"本部級",number:18,pTime:2},
+                    {title:"支部級",number:22,pTime:5},
+                    {title:"地區級",number:63,pTime:16}
+                ],
             }
         },
-        methods: {
-
-        }
         
     }
 </script>
 <template>
     <div>
         <Steps :active="active"></Steps>
-    8
+        <ul>
+            <li v-for="(item) in peopleCount" :key="item.title">
+                {{ item.title}} : {{ item.number }} 人 +( {{ item.pTime}} )
+            </li>
+        </ul>
     </div>
 </template>
 <style lang="scss" scoped>
